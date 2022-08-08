61ed05a9f8d2546831a37e9366485c3dc2f4e66c
Alex Eyler <alexey@microsoft.com>
2022-08-08 12:27:54 -0700

    Init for example

---
+++ Other/ServiceImpl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Other
+{
+    public interface IMyService : IService
+    {
+        Task<int> HelloWorldAsync();
+    }
+
+    internal class ServiceImpl : IMyService
+    {
+        public Task<int> HelloWorldAsync()
+        {
+            ServiceEventSource.Current.Message("Hello world");
+            return Task.FromResult(1);
+        }
+    }
+}
